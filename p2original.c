62b99c5a8bbd229d7cf5d7f9536ca9158e71e39c
Binith001 <95204552+Binith001@users.noreply.github.com>
2021-12-31 09:06:04 +0000

    done

---
+++ p2original.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+void input(int *a, int *b)
+{
+  printf("enter num:");
+  scanf("%d%d",a,b);
+}
+
+void add (int a,int b)
+{
+  *sum = a+b;
+}
+void output(int a,int b,int sum)
+{
+  printf("the sum of &d and &d = &d",a,b,sum);
+}
+int main()
+{
+  int a,b,sum;
+  input(&a,&b);
+  add(a,b,&sum);
+  output(a,b,sum);
+  return 0;
+}
