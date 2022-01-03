836081c9ebe44c39c4a05d39d5646d9c47cfb363
Binith001 <95204552+Binith001@users.noreply.github.com>
2022-01-03 06:47:35 +0000

    p5original.c

--- p2original.c
+++ p2original.c
@@ -7,17 +7,18 @@ void input(int *a, int *b)
 
 void add (int a,int b)
 {
+  int *sum=0;
   *sum = a+b;
 }
-void output(int a,int b,int sum)
+  int output(int a,int b,int sum)
 {
-  printf("the sum of &d and &d = &d",a,b,sum);
+  printf("the sum of %d and %d = %d",a,b,sum);
 }
 int main()
 {
   int a,b,sum;
   input(&a,&b);
-  add(a,b,&sum);
-  output(a,b,sum);
+  add(a,b);
+  output(a, b, &sum);
   return 0;
 }
